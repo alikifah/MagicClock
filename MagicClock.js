1863696554ec95e041864db783350f2e949c3f55
Ali Kifah <olver422@yahoo.com>
2022-08-11 20:20:48 +0200

    Update MagicClock.js

--- MagicClock.js
+++ MagicClock.js
@@ -1,15 +1,14 @@
 
 
 // ==========================================================================================================
-//    				MagicClock
+//    		MagicClock
 //	Author: Al-Khafaji, Ali Kifah
 //	Date:   11.08.2022
 //  Description: A simple, yet powerfull and easy modifiable analog clock for you website, made with pure Javascript
 // ===========================================================================================================
 
 class MagicClock {
-    #clockCentralX = 100;
-    #clockCentralY = 100;
+    #clockRadius = 0;
     #hours;
     #minutes;
     #seconds;
@@ -27,13 +26,20 @@ class MagicClock {
     #minuteHandColor = 'Black';
     #hourHandColor = 'Black';
     #backgroundColor = '#fffff';
-
-    constructor(canvas, theme = null) {
-        this.#canvas = canvas;
-        this.#ctx = canvas.getContext("2d");
+    #secondMinuteHandRotationCorrection = -15;
+    #hourHandRotationCorrection = -3;
+	#shadowWidth= 5;
+	#frameWidth=10;
+	
+    constructor(PositionX, PositionY, width, theme = null) {
+        if (width < 220)
+            width = 220;
+        this.#canvas = this.#initCanvas(PositionX, PositionY, width);
+        this.#ctx = this.#canvas.getContext("2d");
         if (!this.#isValidTheme(theme)) {
             this.#loadColors("Black", "Black", "Black", "White");
             this.#initializeClockBasic();
+            prt(2222);
         }
         else {
             if (theme["secondHandColor"] != undefined)
@@ -43,6 +49,19 @@ class MagicClock {
         }
     }
 
+    #initCanvas(canvasX, canvasY, w) {
+        let canvas = document.createElement('canvas');
+        canvas.id = "clockCanvas";
+        canvas.width = w;
+        canvas.height = w;
+        canvas.style.position = "absolute";
+        document.body.appendChild(canvas);
+        this.#clockRadius = canvas.width / 2;
+        canvas.style.left = canvasX + "px";
+        canvas.style.top = canvasY + "px";
+        canvas.style.position = "absolute";
+        return canvas;
+    }
     #isValidTheme(theme) {
         if (this.#isObject(theme)) {
             if ((this.#isValidColor(theme["secondHandColor"]) &&
@@ -50,24 +69,22 @@ class MagicClock {
                 this.#isValidColor(theme["secondHandColor"]) &&
                 this.#isValidColor(theme["secondHandColor"])
             )
-            ||
-            (theme["secondHandSrc"] !== undefined &&
-                theme["minuteHandSrc"] !== undefined &&
-                theme["hourHandSrc"] !== undefined &&
-                theme["backgroundSrc"] !== undefined &&
-                theme["scale"] !== undefined &&
-                theme["handPivotHX"] !== undefined &&
-                theme["handPivotHY"] !== undefined &&
-                theme["handPivotMX"] !== undefined &&
-                theme["handPivotMY"] !== undefined &&
-                theme["handPivotSX"] !== undefined &&
-                theme["handPivotSY"] !== undefined
-            ))
-            return true;
+                ||
+                (theme["secondHandSrc"] !== undefined &&
+                    theme["minuteHandSrc"] !== undefined &&
+                    theme["hourHandSrc"] !== undefined &&
+                    theme["backgroundSrc"] !== undefined &&
+                    theme["handPivotHX"] !== undefined &&
+                    theme["handPivotHY"] !== undefined &&
+                    theme["handPivotMX"] !== undefined &&
+                    theme["handPivotMY"] !== undefined &&
+                    theme["handPivotSX"] !== undefined &&
+                    theme["handPivotSY"] !== undefined
+                ))
+                return true;
         }
         return false;
     }
-
 
     #loadTheme(theme, onLoadCallback, onerrorCallback) {
         this.#loader = new imageLoader(onLoadCallback, onerrorCallback);
@@ -75,24 +92,22 @@ class MagicClock {
         let minuteImgSrc = theme["minuteHandSrc"];
         let secondImgSrc = theme["secondHandSrc"];
         let backgroundImgSrc = theme["backgroundSrc"];
-        this.#scale = theme["scale"];
         this.#pivotPointHX = theme["handPivotHX"];
         this.#pivotPointHY = theme["handPivotHY"];
-
         this.#pivotPointMX = theme["handPivotMX"];
         this.#pivotPointMY = theme["handPivotMY"];
-
         this.#pivotPointSX = theme["handPivotSX"];
         this.#pivotPointSY = theme["handPivotSY"];
-
         this.#loader.add('secondHandImg', secondImgSrc);
         this.#loader.add('minuteHandImg', minuteImgSrc);
         this.#loader.add('hourHandImg', hourImgSrc);
         this.#loader.add('BGImg', backgroundImgSrc);
     }
-
     #onError() { this.#initializeClockBasic(); }
     #loadingComplete() {
+        // get scale from loaded background
+        let bgImg = this.#loader.get('BGImg');
+        this.#scale = this.#canvas.width / bgImg.width;
         this.#loop();
         setInterval(this.#loop.bind(this), 1000);
     }
@@ -107,10 +122,13 @@ class MagicClock {
         context.restore();
     }
     #renderBg(context) {
+		
         this.#cls();
         context.save();
         this.#shadow(context);
-        this.#drawImage(context, this.#loader.get('BGImg'), 0, 0, this.#scale);
+		context.drawImage(this.#loader.get('BGImg'),  0 , 0,
+		(this.#loader.get('BGImg').width - this.#shadowWidth) * this.#scale ,
+		(this.#loader.get('BGImg').height- this.#shadowWidth) * this.#scale);
         context.restore();
     }
     #loop() {
@@ -120,22 +138,13 @@ class MagicClock {
     }
     #drawClockImage(hour, minute, second) {
         let hoursHandAngle = this.#getClockAngle(hour, true);
-        this.#renderHand(this.#ctx, this.#loader.get('hourHandImg'), this.#pivotPointHX, this.#pivotPointHY, 100, 100, this.#scale, hoursHandAngle);
+        this.#renderHand(this.#ctx, this.#loader.get('hourHandImg'), this.#pivotPointHX, this.#pivotPointHY, this.#clockRadius, this.#clockRadius, this.#scale, hoursHandAngle);
 
         let minutesHandAngle = this.#getClockAngle(minute, false);
-        this.#renderHand(this.#ctx, this.#loader.get('minuteHandImg'), this.#pivotPointMX, this.#pivotPointMY, 100, 100, this.#scale, minutesHandAngle);
+        this.#renderHand(this.#ctx, this.#loader.get('minuteHandImg'), this.#pivotPointMX, this.#pivotPointMY, this.#clockRadius, this.#clockRadius, this.#scale, minutesHandAngle);
 
         let secondsHandAngle = this.#getClockAngle(second, false);
-        this.#renderHand(this.#ctx, this.#loader.get('secondHandImg'), this.#pivotPointSX, this.#pivotPointSY, 100, 100, this.#scale, secondsHandAngle);
-    }
-
-    #getClockAngle(value, isHours = true) {
-        let c = 0;
-        if (isHours)
-            c = 12;
-        else
-            c = 60;
-        return this.#getRad((360 * value) / c);
+        this.#renderHand(this.#ctx, this.#loader.get('secondHandImg'), this.#pivotPointSX, this.#pivotPointSY, this.#clockRadius, this.#clockRadius, this.#scale, secondsHandAngle);
     }
 
     #updateTime() {
@@ -145,11 +154,9 @@ class MagicClock {
         this.#seconds = time.getSeconds();
     }
 
-    #drawImage(context, image, x, y, scale) {
-        context.drawImage(image, 0, 0, image.width * scale, image.height * this.#scale);
-    }
-
-    //Basic themes... (no external images)
+    //#################################################################
+    //#########   Basic themes... (no external images) ##################
+    //#################################################################
     #loadThemeBasic(theme) {
         if (theme === null || theme === undefined) {
             this.#onErrorbasicTheme();
@@ -162,13 +169,10 @@ class MagicClock {
             }
         }
         this.#loadColors(theme["hourHandColor"], theme["minuteHandColor"], theme["secondHandColor"], theme["backgroundColor"]);
-
         this.#initializeClockBasic();
-
     }
 
     #onErrorbasicTheme() {
-
         this.#loadColors("Black", "Black", "Black", "White");
         this.#initializeClockBasic();
     }
@@ -189,53 +193,55 @@ class MagicClock {
     }
 
     #drawClockBasic(hour, minute, second) {
-        hour -= 3;
-        minute -= 3;
-        second += 3;
+        hour += this.#hourHandRotationCorrection;
+        minute += this.#secondMinuteHandRotationCorrection;
+        second += this.#secondMinuteHandRotationCorrection;
         this.#bGPrimitive();
         this.#frame();
         let h = this.#getClockAngle(hour);
         let m = this.#getClockAngle(minute, false);
         let s = this.#getClockAngle(second, false);
 
-        let xH = this.#getCircleX(h, 60);
-        let yH = this.#getCircleY(h, 60);
+        let xH = this.#getCircleX(h, this.#clockRadius * 0.6);
+        let yH = this.#getCircleY(h, this.#clockRadius * 0.6);
 
-        let xM = this.#getCircleX(m, 70);
-        let yM = this.#getCircleY(m, 70);
+        let xM = this.#getCircleX(m, this.#clockRadius * 0.7);
+        let yM = this.#getCircleY(m, this.#clockRadius * 0.7);
 
-        let xS = this.#getCircleX(s, 80);
-        let yS = this.#getCircleY(s, 80);
+        let xS = this.#getCircleX(s, this.#clockRadius * 0.75);
+        let yS = this.#getCircleY(s, this.#clockRadius * 0.75);
 
-        this.#line(0 + this.#clockCentralX, 0 + this.#clockCentralY, xH + this.#clockCentralX, yH + this.#clockCentralY, 6, this.#hourHandColor);
-        this.#line(0 + this.#clockCentralX, 0 + this.#clockCentralY, xM + this.#clockCentralX, yM + this.#clockCentralY, 4, this.#minuteHandColor);
-        this.#line(0 + this.#clockCentralX, 0 + this.#clockCentralY, xS + this.#clockCentralX, yS + this.#clockCentralY, 1, this.#secondHandColor);
+        this.#line(0 + this.#clockRadius, 0 + this.#clockRadius, xH + this.#clockRadius, yH + this.#clockRadius, 6, this.#hourHandColor);
+        this.#line(0 + this.#clockRadius, 0 + this.#clockRadius, xM + this.#clockRadius, yM + this.#clockRadius, 4, this.#minuteHandColor);
+        this.#line(0 + this.#clockRadius, 0 + this.#clockRadius, xS + this.#clockRadius, yS + this.#clockRadius, 1, this.#secondHandColor);
 
-        this.#circle(100, 100, 5, 1, "black", true);
-        // draw hour markers 
-        this.#drawHoursMarker(12, 80, 4);
-        this.#drawHoursMarker(3, 80, 4);
-        this.#drawHoursMarker(6, 80, 4);
-        this.#drawHoursMarker(9, 80, 4);
-        this.#drawHoursMarker(1, 80, 2);
-        this.#drawHoursMarker(2, 80, 2);
-        this.#drawHoursMarker(4, 80, 2);
-        this.#drawHoursMarker(5, 80, 2);
-        this.#drawHoursMarker(7, 80, 2);
-        this.#drawHoursMarker(8, 80, 2);
-        this.#drawHoursMarker(10, 80, 2);
-        this.#drawHoursMarker(11, 80, 2);
-    }
-    #drawHoursMarker(hour, radius, size = 2, color = "Black") {
-        hour -= 3;
-        let angle = this.#getClockAngle(hour);
-        let x = this.#getCircleX(angle, radius);
-        let y = this.#getCircleY(angle, radius);
-        this.#circle(x + this.#clockCentralX, y + this.#clockCentralY, size, 1, color, true);
+        this.#circle(this.#clockRadius, this.#clockRadius, 5, 1, "black", true);
+        // draw markers 
+        let radius = (this.#clockRadius -  this.#shadowWidth - this.#frameWidth) * 0.9;
+        for (let i = 0; i < 60; i++) {
+            if (i % 15 === 0)
+                this.#drawMarker(i, radius, 5);
+            else if (i % 5 === 0)
+                this.#drawMarker(i, radius, 3);
+            else
+                this.#drawMarker(i, radius, 1);
+        }
     }
 
-    #bGPrimitive() { this.#circle(100, 100, 99, 1, this.#backgroundColor, true); }
-    #frame() { this.#circle(100, 100, 95, 10); }
+    #drawMarker(seconds, radius, size = 2, color = "Black") {
+        seconds -= 15;
+        let angle = this.#getClockAngle(seconds, false);
+        let x = this.#getCircleX(angle, radius );
+        let y = this.#getCircleY(angle, radius );
+        this.#circle(x + this.#clockRadius, y + this.#clockRadius, size, 1, color, true);
+    }
+
+    #bGPrimitive() { this.#circle(this.#clockRadius, this.#clockRadius, this.#clockRadius - this.#shadowWidth - this.#frameWidth, 1, this.#backgroundColor, true); }
+    #frame() {
+	this.#circle(this.#clockRadius, this.#clockRadius,
+	this.#clockRadius - this.#shadowWidth - this.#frameWidth , 
+	this.#frameWidth); 
+	}
 
     #line(startX, startY, endX, endY, thickness = 1, color = "black") {
         this.#ctx.save();
@@ -251,7 +257,7 @@ class MagicClock {
     }
     #shadow(context, color = "Black") {
         this.#ctx.shadowColor = color;
-        this.#ctx.shadowBlur = 5;
+        this.#ctx.shadowBlur = this.#shadowWidth ;
         this.#ctx.shadowOffsetX = 0;
         this.#ctx.shadowOffsetY = 2;
     }
@@ -274,8 +280,15 @@ class MagicClock {
         }
         this.#ctx.restore();
     }
-
-    #cls() { this.#ctx.clearRect(0, 0, canvas.width, canvas.height); }
+    #getClockAngle(value, isHours = true) {
+        let c = 0;
+        if (isHours)
+            c = 12;
+        else
+            c = 60;
+        return this.#getRad((360 * value) / c);
+    }
+    #cls() { this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height); }
     #getRad(grad) { return grad * (Math.PI / 180); }
 
     #getCircleX(radians, radius) { return Math.cos(radians) * radius; }
@@ -332,3 +345,6 @@ class imageLoader {
 
 
 
+function prt(o) {
+    console.log(o);
+}
